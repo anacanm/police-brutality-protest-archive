6f6cd2841e19ccb1cda8e64e606f3b2b052d29f5
anacanm <anacan@mangelsdorf.io>
2020-06-17 14:53:25 -0700

    Project created with mix new

---
+++ protest_archive/lib/protest_archive.ex
@@ -0,0 +1,18 @@
+defmodule ProtestArchive do
+  @moduledoc """
+  Documentation for `ProtestArchive`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProtestArchive.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ protest_archive/test/protest_archive_test.exs
@@ -0,0 +1,8 @@
+defmodule ProtestArchiveTest do
+  use ExUnit.Case
+  doctest ProtestArchive
+
+  test "greets the world" do
+    assert ProtestArchive.hello() == :world
+  end
+end
