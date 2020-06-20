bf1309702ae31780a68fbafb730088f40cbdefa0
anacanm <anacan@mangelsdorf.io>
2020-06-20 14:54:50 -0700

    Add specs and documentation to collect_helper

--- protest_archive/lib/protest_archive/collect_helper.ex
+++ protest_archive/lib/protest_archive/collect_helper.ex
@@ -1,6 +1,7 @@
 defmodule ProtestArchive.CollectHelper do
   # API ##########################################################################
 
+  @spec get_data!(tuple, number | String.t(), String.t()) :: list(map) | map
   def get_data!({type, tag}, num_results, from) do
     url({type, tag}, num_results, from)
     |> fetch_data!(type)
@@ -9,6 +10,7 @@ defmodule ProtestArchive.CollectHelper do
 
   # Helpers #######################################################################
 
+  @spec fetch_data!(String.t(), atom) :: %HTTPoison.Response{}
   defp fetch_data!(url, :news) when is_bitstring(url) do
     # the news api stores the auth key in the url, so no headers are needed
     url
@@ -27,11 +29,13 @@ defmodule ProtestArchive.CollectHelper do
 
   # building url #######################################################
 
+  @spec url(tuple, number, String.t()) :: String.t()
   def url({type, tag}, num_results, from) do
     base_url(type, num_results, from)
     |> add_tag(type, tag)
   end
 
+  @spec base_url(atom, number | String.t(), String.t()) :: String.t()
   defp base_url(:news, num_results, _from = nil) do
     # if no _from is provided (default val is nil), let news api calculate oldest date
     "https://newsapi.org/v2/everything?language=en&pageSize=#{num_results}&apiKey=#{
@@ -49,6 +53,7 @@ defmodule ProtestArchive.CollectHelper do
     "https://api.twitter.com/1.1/search/tweets.json?lang=en&count=#{num_results}&"
   end
 
+  @spec add_tag(String.t(), atom, String.t()) :: String.t()
   defp add_tag(url, _type = :news, tag) when is_bitstring(tag) do
     url <> "q=#{encode_tag(tag)}"
   end
@@ -59,6 +64,7 @@ defmodule ProtestArchive.CollectHelper do
     url <> "q=#{encode_tag("#" <> tag)}"
   end
 
+  @spec add_tag(String.t(), atom, list(String.t())) :: String.t()
   defp add_tag(url, _type = :tweet, tag) when is_list(tag) do
     url <> "q=#{encode_queries(tag)}"
   end
@@ -66,7 +72,7 @@ defmodule ProtestArchive.CollectHelper do
   # working with response ########################
 
   defp decode_response(response, _type = :news) do
-    response |> handle_decode()
+    response |> handle_decode() |> change_sources_to_string()
   end
 
   defp decode_response(response, _type = :tweet) do
@@ -76,18 +82,30 @@ defmodule ProtestArchive.CollectHelper do
     |> hd()
   end
 
+  # handle_decode returns a map if the type is a tweet, or a list of map if the type is news
+  @spec handle_decode(%HTTPoison.Response{}) :: map | list(map)
   defp handle_decode(response) do
     response
     |> Map.fetch!(:body)
     |> Poison.decode!()
   end
 
+  @spec change_sources_to_string(map) :: list(map)
+  defp change_sources_to_string(response) when is_map(response) do
+    response["articles"]
+    |> Enum.map(fn article ->
+      Map.update(article, "source", "", fn source -> source["name"] end)
+    end)
+  end
+
   # Helpers ###########################################################
 
+  @spec encode_tag(String.t()) :: String.t()
   defp encode_tag(tag) do
     tag |> String.trim() |> String.downcase() |> URI.encode_www_form()
   end
 
+  @spec encode_queries(list(String.t()), String.t()) :: String.t()
   defp encode_queries([head | tail], operator \\ "AND") do
     operator = String.upcase(operator)
 
