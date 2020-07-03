1817774462ad478d76d06a04c931d42455460a0f
anacanm <anacan@mangelsdorf.io>
2020-07-03 12:53:41 -0700

    Add Ecto Tweet changeset

--- protest_archive/lib/protest_archive/tweet.ex
+++ protest_archive/lib/protest_archive/tweet.ex
@@ -12,4 +12,15 @@ defmodule ProtestArchive.Tweet do
     field(:url, :string)
     field(:url_to_profile_image, :string)
   end
+
+  @spec changeset(%ProtestArchive.Tweet{}, map) :: term
+  def changeset(tweet, params \\ %{}) do
+    fields = [:author, :author_handle, :content, :published_at, :tag, :tweet_id, :url, :url_to_profile_image]
+    required_fields = [:author, :author_handle, :content, :published_at, :tag, :tweet_id, :url]
+
+    tweet
+    |> Ecto.Changeset.cast(params, fields)
+    |> Ecto.Changeset.validate_required(required_fields)
+    |> Ecto.Changeset.unique_constraint(:tweet_id)
+  end
 end
