1dca289ea83d7448571a78fd5d4e95b4fd2f6442
anacanm <anacan@mangelsdorf.io>
2020-07-04 16:42:18 -0700

    Cache reads from DB || API if state == [] and on init

--- protest_archive/lib/protest_archive/cache.ex
+++ protest_archive/lib/protest_archive/cache.ex
@@ -1,14 +1,17 @@
 defmodule ProtestArchive.Cache do
   use GenServer, restart: :transient
-  alias ProtestArchive.ProcessRegistry
+  alias ProtestArchive.{ProcessRegistry, CollectHelper, DatabaseWorker}
 
   def start_link(name = {type, tag}) do
     IO.inspect("Starting cache: type: #{type} tag: #{tag}")
-    GenServer.start_link(__MODULE__, [], name: via_tuple(name))
+    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
   end
 
-  def get(server) do
-    GenServer.call(via_tuple(server), :get)
+  @doc """
+  the server param is a {type, tag} tuple
+  """
+  def get(server = {type, tag}) do
+    GenServer.call(via_tuple(server), {:get, type, tag})
   end
 
   def put(server, new_state) do
@@ -18,14 +21,16 @@ defmodule ProtestArchive.Cache do
   # Server
 
   @impl true
-  def init(state \\ []) do
-    # TODO: on init, send message to self to read from DB or query api
-    {:ok, state}
+  def init({type, tag}) do
+    send(self(), :init_state)
+    # name is used as the inital state so that when calling :init_state, it can load state with the correct values
+    {:ok, {type, tag}}
   end
 
   @impl true
-  def handle_call(:get, _from, state) do
-    # TODO: if state == [], query from DB. if DB is empty, query api
+  def handle_call({:get, type, tag}, _from, state) do
+    state = handle_state(type, tag, state)
+
     {:reply, state, state}
   end
 
@@ -33,6 +38,33 @@ defmodule ProtestArchive.Cache do
   def handle_cast({:put, new_state}, state) do
     # if the new state is not good, preserve the old state
     {:noreply, new_state || state}
+  end
+
+  @impl true
+  def handle_info(:init_state, _state = {type, tag}) do
+    new_state = handle_state(type, tag, [])
+
+    {:noreply, new_state}
+  end
+
+  #################################
+
+  # if state == [], read from DB. if DB, is empty, query API
+  @spec handle_state(atom, String.t(), list()) :: list(map)
+  defp handle_state(type, tag, state) do
+    cond do
+      state != [] ->
+        state
+
+      true ->
+        case DatabaseWorker.get_tag(type, tag, "published_at") do
+          [] ->
+            CollectHelper.get_data!({type, tag}, 25, "recent")
+
+          new_state_from_db ->
+            new_state_from_db
+        end
+    end
   end
 
   ##########################
